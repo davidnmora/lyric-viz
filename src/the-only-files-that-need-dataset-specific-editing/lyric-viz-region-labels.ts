eae11c62a00853c2d9641585afc4f1024b2c178a
David Mora <davidnmora@gmail.com>
2023-02-12 18:55:24 -0800

    Add labels

--- src/the-only-files-that-need-dataset-specific-editing/lyric-viz-region-labels.ts
+++ src/the-only-files-that-need-dataset-specific-editing/lyric-viz-region-labels.ts
@@ -21,6 +21,81 @@ const RAW_LABELS_NOT_IN_GEOJSON_FORM: Array<RawLabel> = [
   },
 
   {
+    label: "Chance & Risk",
+    nearestLyric: "I'll take my share of chances",
+    coordinates: [65.48275756835938, 37.49410629272461],
+    children: [
+      {
+        label: "'risk'",
+        nearestLyric: "The risk that I'm takin'",
+        coordinates: [65.2879638671875, 37.3307991027832],
+      },
+
+      {
+        label: "'take a chance'",
+        nearestLyric: "(Take a chance, take a chance, take a chance on me)",
+        coordinates: [65.6971435546875, 37.244544982910156],
+      },
+
+      {
+        label: "'one more chance'",
+        nearestLyric: "Won't you just give me one chance?",
+        coordinates: [65.77791595458984, 37.15735626220703],
+      },
+      {
+        label: "'second chance'",
+        nearestLyric: "There ain't no second chance",
+        coordinates: [65.94288635253906, 36.92943572998047],
+      },
+    ],
+  },
+
+  {
+    label: "Say and Tell",
+    nearestLyric: "Just one thing to say",
+    coordinates: [69.08057403564453, 41.838905334472656],
+    children: [
+      {
+        label: "Conversation",
+        nearestLyric: "But conversation couldn't make it through",
+        coordinates: [66.99437713623047, 39.814971923828125],
+        children: [
+          {
+            label: "'listen'",
+            nearestLyric: "Listen baby",
+            coordinates: [66.36129760742188, 38.42033386230469],
+          },
+
+          {
+            label: "'talk to me'",
+            nearestLyric: "Talk to me",
+            coordinates: [67.65168762207031, 38.83012390136719],
+          },
+        ],
+      },
+
+      {
+        label: "'nothing to say'",
+        nearestLyric: "And I ain't got nothing to say",
+        coordinates: [68.20171356201172, 42.13831329345703],
+      },
+
+      {
+        label: "'words'",
+        nearestLyric: "To say these words to you",
+        coordinates: [68.03060150146484, 41.103187561035156],
+        children: [
+          {
+            label: "'word' metaphors & descriptions",
+            nearestLyric: "After all your words of steel",
+            coordinates: [67.89266967773438, 41.21478271484375],
+          },
+        ],
+      },
+    ],
+  },
+
+  {
     label: "'You and I/me'",
     nearestLyric: "Just you and I",
     coordinates: [71.42050266265869, 54.23489809036255],
@@ -30,6 +105,171 @@ const RAW_LABELS_NOT_IN_GEOJSON_FORM: Array<RawLabel> = [
     label: "Don't care",
     nearestLyric: "But you don't care",
     coordinates: [69.3930983543396, 57.17419981956482],
+  },
+
+  {
+    label: "'no limmit'",
+    nearestLyric: "Told ya'll there wasn't no limit",
+    coordinates: [76.40230560302734, 47.48091125488281],
+  },
+
+  {
+    label: "Understanding",
+    nearestLyric: "Can you understand me",
+    coordinates: [73.05299377441406, 46.08429718017578],
+    children: [
+      {
+        label: "'learn'",
+        nearestLyric: "But I'm ready [ready] to learn (to learn)",
+        coordinates: [68.038818359375, 46.574951171875],
+      },
+
+      {
+        label: "'you'll never know'",
+        nearestLyric: "If you cut it down, then you'll never know",
+        coordinates: [69.40914916992188, 45.43326950073242],
+      },
+
+      {
+        label: "'you should know'",
+        nearestLyric: "You should know yeah",
+        coordinates: [69.8878402709961, 45.330413818359375],
+      },
+
+      {
+        label: "questions & answers",
+        nearestLyric: "All the answers to my questions, I have to find",
+        coordinates: [71.3383560180664, 45.04317092895508],
+      },
+
+      {
+        label: "'don't know'",
+        nearestLyric: "I just don't know, I don't know, I don't know-oh",
+        coordinates: [72.0332260131836, 44.49148941040039],
+      },
+
+      {
+        label: "'I don't understand'",
+        nearestLyric: "I don't understand",
+        coordinates: [72.89093017578125, 45.73036575317383],
+      },
+
+      {
+        label: "'confused'",
+        nearestLyric: "Has got me so confused",
+        coordinates: [72.62840270996094, 47.04587173461914],
+      },
+
+      {
+        label: "'mystery'",
+        nearestLyric: "I've been so into your mystery",
+        coordinates: [73.9606704711914, 46.31266403198242],
+      },
+    ],
+  },
+
+  {
+    label: "'no regrets'",
+    nearestLyric: "No regrets, just love",
+    coordinates: [73.31172180175781, 44.63481903076172],
+  },
+
+  {
+    label: "🤔 Reasoning Right from Wrong",
+    nearestLyric: "I'm telling you baby, I made my mistakes",
+    coordinates: [72.38224029541016, 51.08867263793945],
+    children: [
+      {
+        label: "'reason' & 'why'",
+        nearestLyric: "There's always some reason",
+        coordinates: [74.4483642578125, 48.711910247802734],
+        children: [
+          {
+            label: "'reasons why'",
+            nearestLyric: "Don't give me no reasons why",
+            coordinates: [74.37449645996094, 48.60906219482422],
+          },
+
+          {
+            label: "'tell me why'",
+            nearestLyric: "Tell me why",
+            coordinates: [74.07942962646484, 48.44374465942383],
+          },
+
+          {
+            label: "'don't know why'",
+            nearestLyric: "And I don't know why",
+            coordinates: [74.03987884521484, 47.847999572753906],
+          },
+        ],
+      },
+
+      {
+        label: "'made a mistake'",
+        nearestLyric: "We only humans, girl, we make mistakes",
+        coordinates: [73.2662582397461, 51.6363410949707],
+      },
+
+      {
+        label: "'compromise'",
+        nearestLyric: "Tryna compromise, but I can’t win",
+        coordinates: [73.04209899902344, 52.568572998046875],
+      },
+
+      {
+        label: "'intentions'",
+        nearestLyric: "Yeah, these are my only intentions",
+        coordinates: [72.36034393310547, 52.01563262939453],
+      },
+
+      {
+        label: "'(dis)agree",
+        nearestLyric: "I'm sure all would agree",
+        coordinates: [72.73835754394531, 50.65395736694336],
+      },
+      {
+        label: "'don't mean nothing'",
+        nearestLyric: "(Don't mean nothing)",
+        coordinates: [70.58865356445312, 51.80863952636719],
+      },
+
+      {
+        label: "'right' and 'wrong'",
+        nearestLyric: "If you're wrong or if you're right",
+        coordinates: [71.50531005859375, 49.78706359863281],
+        children: [
+          {
+            label: "'right or wrong'",
+            nearestLyric: "Right or wrong (All day, every day, hey)",
+            coordinates: [71.41566467285156, 49.974796295166016],
+          },
+
+          {
+            label: "'I was wrong'",
+            nearestLyric: "Too proud to tell you I was wrong",
+            coordinates: [71.68643951416016, 49.739707946777344],
+          },
+
+          {
+            label: "'Am I wrong?'",
+            nearestLyric: "Am I wrong?",
+            coordinates: [71.86373138427734, 49.813262939453125],
+          },
+
+          {
+            label: "something/nothing wrong",
+            nearestLyric: "'Cause there's nothing wrong with me",
+            coordinates: [70.31385803222656, 49.78252410888672],
+          },
+        ],
+      },
+
+      {
+        label: "'prove it'",
+        nearestLyric: "Prove it",
+        coordinates: [71.6731185913086, 50.276771545410156],
+      },
+    ],
   },
 
   {
@@ -385,6 +625,7 @@ const getFlatListFromList = (
   );
 
 const MAX_FONT_SIZE = 16;
+const FONT_SIZES_BY_LEVEL = ["nothing", 16, 10, 8, 6, 5];
 const LYRIC_VIZ_REGION_LABELS: Object = {
   type: "FeatureCollection",
   name: "lyric_viz_region_labels",
@@ -395,7 +636,7 @@ const LYRIC_VIZ_REGION_LABELS: Object = {
         label,
         level,
         nearestLyric,
-        size: level ? MAX_FONT_SIZE / level : MAX_FONT_SIZE, // lower levels are smaller
+        size: FONT_SIZES_BY_LEVEL[level || 1] || MAX_FONT_SIZE, // lower levels are smaller
       },
       geometry: {
         type: "Point",
